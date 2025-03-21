e5f04819f30ed708e63bb13833b7d1a4b07e334e
Ivan de Araujo <ivanmartinezoficial@gmail.com>
2025-03-21 15:10:38 -0300

    ultima implementaçao

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Account } from './account'
 
-const a1 = new Account(123000, 25, "fulano");
-const a2 = new Account(1552811, 25, "beltrano");
-const a3 = new Account(32345222, 25, "cicrano");
+const a1 = new Account(123000, 25, "fulano de tal");
+const a2 = new Account(1552811, 62, "beltrano fela da gaita");
+const a3 = new Account(32345222, 37, "cicrano beltrano");
 
 a1.deposit(1000.000)
 a1.withdraw(200.00)
